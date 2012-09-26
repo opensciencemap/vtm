93d57c2577beb5030ab7d0913bf406940148ecd1
Hannes Janetzek <hannes.janetzek@gmail.com>
2012-09-26 19:54:37 +0200

    add copyright

--- src/org/oscim/view/renderer/ScanBox.java
+++ src/org/oscim/view/renderer/ScanBox.java
@@ -13,7 +13,7 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ported from Polymaps: Layer.js */
+/* ported from Polymaps: Layer.js Copyright (c) 2010, SimpleGeo and Stamen Design */
 
 package org.oscim.view.renderer;
 
