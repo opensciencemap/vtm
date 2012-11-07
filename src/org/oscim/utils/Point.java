695c1532d6bcd2c3c8c48dcab91c973a11d2f65e
Hannes Janetzek <hannes.janetzek@gmail.com>
2012-11-07 23:16:54 +0100

    a point, with floats

---
+++ src/org/oscim/utils/Point.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2012 Hannes Janetzek
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.oscim.utils;
+
+/**
+ * without further ado: This class represents a 2D Point using float coordinates
+ */
+public class Point {
+	public float x;
+	public float y;
+
+	Point(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	Point() {
+	}
+}
