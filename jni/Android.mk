eb59194e637e95f231a5b77596135269a5881259
jeff <hannes.janetzek@gmail.com>
2013-01-04 09:49:41 +0100

    add triangle jni source

---
+++ jni/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+# TRILIBDEFS = -DTRILIBRARY -DREDUCED -DCDT_ONLY
+LOCAL_CFLAGS := -O -DTRILIBRARY -DREDUCED -DCDT_ONLY -Werror
+# -DLINUX -> no fpu_control in bionic, needed ?
+
+LOCAL_MODULE    := triangle-jni
+LOCAL_SRC_FILES := TriangleJni.c triangle.c
+LOCAL_LDLIBS    := -llog
+
+include $(BUILD_SHARED_LIBRARY)
