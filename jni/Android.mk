8d219560fb46bd450341c749cf35e4e3a5fe266c
jeff <hannes.janetzek@gmail.com>
2013-01-13 13:49:04 +0100

    no timing required

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 # TRILIBDEFS = -DTRILIBRARY -DREDUCED -DCDT_ONLY
-LOCAL_CFLAGS := -O -DTRILIBRARY -DREDUCED -DCDT_ONLY -Werror
+LOCAL_CFLAGS := -O -DTRILIBRARY -DREDUCED -DCDT_ONLY -DNO_TIMER -Werror
 # -DLINUX -> no fpu_control in bionic, needed ?
 
 LOCAL_MODULE    := triangle-jni
