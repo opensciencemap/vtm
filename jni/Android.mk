55d70cf9d65265fb02fbb6605d7171e60d6c639b
hjanetzek <hannes.janetzek@gmail.com>
2013-03-15 05:28:17 +0100

    - add jni gl matrix utiles - move Triangle jni to subdir

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,7 +8,17 @@ LOCAL_CFLAGS := -O -DTRILIBRARY -DREDUCED -DCDT_ONLY -DNO_TIMER -Werror -std=c99
 # -DLINUX -> no fpu_control in bionic, needed ?
 
 LOCAL_MODULE    := triangle-jni
-LOCAL_SRC_FILES := TriangleJni.c triangle.c
+LOCAL_SRC_FILES := triangle/TriangleJni.c triangle/triangle.c
 LOCAL_LDLIBS    := -llog
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := glutils
+#LOCAL_ARM_MODE := arm
+LOCAL_CFLAGS    := -Werror -O2 -ffast-math -std=c99
+LOCAL_SRC_FILES := gl/utils.c
+LOCAL_LDLIBS    := -llog -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
