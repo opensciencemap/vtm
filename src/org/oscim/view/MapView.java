205149c3974b175817bfdff802b957e8cd8cce55
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-06-15 16:46:15 +0200

    always add event layer

--- src/org/oscim/view/MapView.java
+++ src/org/oscim/view/MapView.java
@@ -23,8 +23,8 @@ import org.oscim.core.MapPosition;
 import org.oscim.core.Tile;
 import org.oscim.layers.Layer;
 import org.oscim.layers.MapEventLayer;
+import org.oscim.layers.labeling.LabelLayer;
 import org.oscim.layers.overlay.BuildingOverlay;
-import org.oscim.layers.overlay.LabelingOverlay;
 import org.oscim.layers.overlay.Overlay;
 import org.oscim.layers.tile.bitmap.BitmapTileLayer;
 import org.oscim.layers.tile.vector.MapTileLayer;
@@ -134,6 +134,8 @@ public class MapView extends RelativeLayout {
 
 		addView(mGLView, params);
 
+		mLayerManager.add(0, new MapEventLayer(this));
+
 		clearMap();
 		redrawMap(false);
 	}
@@ -143,15 +145,13 @@ public class MapView extends RelativeLayout {
 
 		baseLayer.setTileSource(tileSource);
 
-		mLayerManager.add(0, new MapEventLayer(this));
-
 		mLayerManager.add(1, baseLayer);
 
 		mRotationEnabled = true;
 
 		//mLayerManager.add(new GenericOverlay(this, new GridRenderLayer(this)));
 		mLayerManager.add(new BuildingOverlay(this, baseLayer.getTileLayer()));
-		mLayerManager.add(new LabelingOverlay(this, baseLayer.getTileLayer()));
+		mLayerManager.add(new LabelLayer(this, baseLayer.getTileLayer()));
 
 		return baseLayer;
 	}
