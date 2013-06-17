8f456c589429fd1b76c8fcec37cbccd927fe7e2c
hjanetzek <hannes.janetzek@gmail.com>
2013-06-17 05:36:55 +0200

    default to max zoom 20

--- src/org/oscim/view/MapViewPosition.java
+++ src/org/oscim/view/MapViewPosition.java
@@ -32,7 +32,7 @@ public class MapViewPosition {
 	private static final String TAG = MapViewPosition.class.getName();
 
 	// needs to fit for int: 2 * 20 * Tile.SIZE
-	public final static int MAX_ZOOMLEVEL = 24;
+	public final static int MAX_ZOOMLEVEL = 20;
 	public final static int MIN_ZOOMLEVEL = 2;
 
 	public final static double MAX_SCALE = (1 << MAX_ZOOMLEVEL);
