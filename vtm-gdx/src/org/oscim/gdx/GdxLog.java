0e8def6a828652af7a5818d5a1ca6eadd7daeaf5
hjanetzek <hannes.janetzek@gmail.com>
2013-06-26 12:05:09 +0200

    fixme: debugging

--- vtm-gdx/src/org/oscim/gdx/GdxLog.java
+++ vtm-gdx/src/org/oscim/gdx/GdxLog.java
@@ -2,29 +2,33 @@ package org.oscim.gdx;
 
 import org.oscim.backend.Log.Logger;
 
-import com.badlogic.gdx.Gdx;
-
 public class GdxLog implements Logger {
 
 	@Override
 	public void d(String tag, String msg) {
-		Gdx.app.log(tag, msg);
-
+		//Gdx.app.log(tag, msg);
+		System.err.println(msg);
 	}
 
 	@Override
 	public void w(String tag, String msg) {
-		Gdx.app.log(tag, msg);
+		//Gdx.app.log(tag, msg);
+		System.err.println(msg);
+
 	}
 
 	@Override
 	public void e(String tag, String msg) {
-		Gdx.app.log(tag, msg);
+		//Gdx.app.log(tag, msg);
+		System.err.println(msg);
+
 	}
 
 	@Override
 	public void i(String tag, String msg) {
-		Gdx.app.log(tag, msg);
+		//Gdx.app.log(tag, msg);
+		System.err.println(msg);
+
 	}
 
 }
