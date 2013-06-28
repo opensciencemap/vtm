0ec39059c8c2c69e4ee4d57142254152a7b92c85
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-06-28 04:32:54 +0200

    use GwtCanvasAdapter

--- vtm-gdx-html/src/org/oscim/gdx/client/GwtGdxMap.java
+++ vtm-gdx-html/src/org/oscim/gdx/client/GwtGdxMap.java
@@ -1,5 +1,6 @@
 package org.oscim.gdx.client;
 
+import org.oscim.backend.CanvasAdapter;
 import org.oscim.backend.GL20;
 import org.oscim.backend.GLAdapter;
 import org.oscim.gdx.GdxMap;
@@ -10,7 +11,13 @@ public class GwtGdxMap extends GdxMap {
 
 	@Override
 	public void create() {
-		GLAdapter.INSTANCE = (GL20)Gdx.graphics.getGL20(); //(GL20)Gdx.gl20;
+		CanvasAdapter.g = GwtCanvasAdapter.INSTANCE;
+		GLAdapter.g = (GL20)Gdx.graphics.getGL20();
+		GLAdapter.GDX_WEBGL_QUIRKS = true;
+		//GLAdapter.NON_PREMUL_CANVAS = true;
+
+		//Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
 		super.create();
 	}
 }
