dccd5943dd78bcceea526c2570fe15e9a405b12a
hjanetzek <hannes.janetzek@gmail.com>
2013-07-01 04:17:53 +0200

    gdx: pass TileSource to GdxMap constructor

--- vtm-gdx-desktop/src/org/oscim/gdx/Main.java
+++ vtm-gdx-desktop/src/org/oscim/gdx/Main.java
@@ -4,6 +4,8 @@ import org.oscim.awt.AwtGraphics;
 import org.oscim.backend.CanvasAdapter;
 import org.oscim.backend.GLAdapter;
 import org.oscim.core.Tile;
+import org.oscim.tilesource.TileSource;
+import org.oscim.tilesource.oscimap4.OSciMap4TileSource;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -17,17 +19,20 @@ public class Main {
 		cfg.useGL20 = true;
 		cfg.width = 1280;
 		cfg.height = 800;
-		cfg.stencil= 8;
+		cfg.stencil = 8;
 		cfg.foregroundFPS = 20;
-		//cfg.samples = 4;
+		// cfg.samples = 4;
 
 		// set our globals
-        CanvasAdapter.g = AwtGraphics.INSTANCE;
-        GLAdapter.g = new GdxGLAdapter();
+		CanvasAdapter.g = AwtGraphics.INSTANCE;
+		GLAdapter.g = new GdxGLAdapter();
 		Tile.SIZE = 256;
 
-        new SharedLibraryLoader().load("vtm-jni");
+		new SharedLibraryLoader().load("vtm-jni");
 
-		new LwjglApplication(new GdxMap(), cfg);
+		TileSource tileSource = new OSciMap4TileSource();
+		tileSource.setOption("url", "http://city.informatik.uni-bremen.de/osci/testing");
+
+		new LwjglApplication(new GdxMap(tileSource), cfg);
 	}
 }
