049751f05f67db4ec8d1e374f01b242ed65a9aa8
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-08-04 14:37:45 +0200

    fix displaced caption labels - use 'dy' in label bounding box

--- vtm/src/org/oscim/utils/OBB2D.java
+++ vtm/src/org/oscim/utils/OBB2D.java
@@ -134,12 +134,17 @@ public class OBB2D {
 		computeAxes();
 	}
 
-	public void setNormalized(float cx, float cy, float vx, float vy, float width, float height) {
+	public void setNormalized(float cx, float cy, float vx, float vy, float width, float height, float dy) {
 		float ux = -vy;
 		float uy = vx;
 
 		float hw = width / 2;
 		float hh = height / 2;
+
+		if (dy != 0){
+			cx += vx * dy + vy * dy;
+			cy += -vy * dy + vx * dy;
+		}
 
 		vx *= hw;
 		vy *= hw;
