d05c4ad0d198a786449bc8e07b5acaf59af7e770
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-09-11 13:53:30 +0200

    return null when asset cannot be read

--- vtm-gdx/src/org/oscim/gdx/GdxAssetAdapter.java
+++ vtm-gdx/src/org/oscim/gdx/GdxAssetAdapter.java
@@ -6,15 +6,20 @@ import org.oscim.backend.AssetAdapter;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 
-public class GdxAssetAdapter extends AssetAdapter{
+public class GdxAssetAdapter extends AssetAdapter {
 
 	@Override
-    public InputStream openFileAsStream(String fileName) {
-		FileHandle file =  Gdx.files.internal(fileName);
+	public InputStream openFileAsStream(String fileName) {
+		FileHandle file = Gdx.files.internal(fileName);
 		if (file == null)
 			throw new IllegalArgumentException("missing file " + fileName);
 
-		return file.read();
-    }
+		try {
+			return file.read();
+		} catch (GdxRuntimeException e) {
+			return null;
+		}
+	}
 }
