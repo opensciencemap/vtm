19b32adc344ae20ce72dce8c75f684850bef0aca
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-09-20 17:05:47 +0200

    add docs

--- vtm/src/org/oscim/backend/AssetAdapter.java
+++ vtm/src/org/oscim/backend/AssetAdapter.java
@@ -2,9 +2,19 @@ package org.oscim.backend;
 
 import java.io.InputStream;
 
+/**
+ * The Class AssetAdapter.
+ */
 public abstract class AssetAdapter {
+
 	public static AssetAdapter g;
 
+	/**
+	 * Open file as stream.
+	 *
+	 * @param name the name
+	 * @return the input stream
+	 */
 	public abstract InputStream openFileAsStream(String name);
 
 }
