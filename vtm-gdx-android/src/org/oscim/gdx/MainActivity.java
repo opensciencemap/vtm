02706d40ef74d88b1fd8d39add1532ebfae871e2
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-11-30 20:51:58 +0100

    fix vtm-gdx-android build

--- vtm-gdx-android/src/org/oscim/gdx/MainActivity.java
+++ vtm-gdx-android/src/org/oscim/gdx/MainActivity.java
@@ -1,14 +1,12 @@
 package org.oscim.gdx;
 
-import org.oscim.android.AndroidLog;
 import org.oscim.android.canvas.AndroidGraphics;
 import org.oscim.backend.CanvasAdapter;
 import org.oscim.backend.GL20;
 import org.oscim.backend.GLAdapter;
-import org.oscim.backend.Log;
 import org.oscim.core.Tile;
 import org.oscim.tiling.source.TileSource;
-import org.oscim.tiling.source.oscimap2.OSciMap2TileSource;
+import org.oscim.tiling.source.oscimap4.OSciMap4TileSource;
 
 import android.os.Bundle;
 
@@ -26,10 +24,8 @@ public class MainActivity extends AndroidApplication {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// set globals
 		CanvasAdapter.g = AndroidGraphics.INSTANCE;
 		GLAdapter.g = new AndroidGLAdapter();
-		Log.logger = new AndroidLog();
 
 		// TODO make this dpi dependent
 		Tile.SIZE = 400;
@@ -43,15 +39,11 @@ public class MainActivity extends AndroidApplication {
 	}
 
 	class GdxMapAndroid extends GdxMap {
-
 		@Override
 		public void createLayers() {
 
-			//TileSource ts = new OSciMap4TileSource();
-			//ts.setOption("url", "http://opensciencemap.org/tiles/vtm");
-
-			TileSource ts = new OSciMap2TileSource();
-			ts.setOption("url", "http://opensciencemap.org/osci/map-live");
+			TileSource ts = new OSciMap4TileSource();
+			ts.setOption("url", "http://opensciencemap.org/tiles/vtm");
 
 			initDefaultLayers(ts, true, true, true);
 		}
