8685b3331b9316befd259ca95908daac1e7bfc7c
Hannes Janetzek <hannes.janetzek@gmail.com>
2014-02-05 04:38:28 +0100

    update copyright, add authors

--- vtm/src/org/oscim/utils/MathUtils.java
+++ vtm/src/org/oscim/utils/MathUtils.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
- * Copyright 2011 libgdx authors.
- *
+ * Copyright 2011 Mario Zechner <badlogicgames@gmail.com>
+ * Copyright 2011 Nathan Sweet <nathan.sweet@gmail.com>
+ *   
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
--- vtm/src/org/oscim/utils/async/AsyncExecutor.java
+++ vtm/src/org/oscim/utils/async/AsyncExecutor.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
- * Copyright 2013 See libgdx AUTHORS file.
- *
+ * Copyright 2013 Mario Zechner <badlogicgames@gmail.com>
+ * Copyright 2013 Nathan Sweet <nathan.sweet@gmail.com>
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
