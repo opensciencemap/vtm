9c138beb5df438c0add3abb8b07d23608e83d692
Hannes Janetzek <hannes.janetzek@gmail.com>
2014-02-24 16:30:10 +0100

    improve DebugTheme

--- vtm/src/org/oscim/theme/DebugTheme.java
+++ vtm/src/org/oscim/theme/DebugTheme.java
@@ -1,13 +1,24 @@
 package org.oscim.theme;
 
+import org.oscim.backend.canvas.Color;
 import org.oscim.core.GeometryBuffer.GeometryType;
 import org.oscim.core.TagSet;
+import org.oscim.theme.styles.Area;
+import org.oscim.theme.styles.Line;
 import org.oscim.theme.styles.RenderStyle;
 
 public class DebugTheme implements IRenderTheme {
 
+	private final static Line[] line = { new Line(1, Color.MAGENTA, 2) };
+	private final static Area[] area = { new Area(0, Color.CYAN) };
+
 	@Override
 	public RenderStyle[] matchElement(GeometryType type, TagSet tags, int zoomLevel) {
+		if (type == GeometryType.LINE)
+			return line;
+		if (type == GeometryType.POLY)
+			return area;
+
 		return null;
 	}
 
