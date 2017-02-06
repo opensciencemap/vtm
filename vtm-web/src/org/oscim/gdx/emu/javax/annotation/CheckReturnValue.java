6d93aaf57fded5520cad5f6395d42ea104e074ff
Emux <devemux86@gmail.com>
2017-02-06 10:57:27 +0200

    Update vtm-web-js build

--- vtm-web/src/org/oscim/gdx/emu/javax/annotation/CheckReturnValue.java
+++ vtm-web/src/org/oscim/gdx/emu/javax/annotation/CheckReturnValue.java
@@ -3,6 +3,6 @@ package javax.annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-@Target( { ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.PACKAGE })
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.PACKAGE})
 public @interface CheckReturnValue {
 }
