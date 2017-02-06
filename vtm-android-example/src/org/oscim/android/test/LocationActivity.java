fdc793d9ec73597880742605469f9cabe0f52e6d
Emux <devemux86@gmail.com>
2017-02-06 14:18:43 +0200

    Samples: fix location layer visibility #32

--- vtm-android-example/src/org/oscim/android/test/LocationActivity.java
+++ vtm-android-example/src/org/oscim/android/test/LocationActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 devemux86
+ * Copyright 2016-2017 devemux86
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
@@ -51,7 +51,6 @@ public class LocationActivity extends SimpleMapActivity implements LocationListe
         super.onPause();
 
         locationManager.removeUpdates(this);
-        locationLayer.setEnabled(false);
     }
 
     @Override
