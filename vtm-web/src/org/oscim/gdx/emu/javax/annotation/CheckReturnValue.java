8fc354c96a5121fa5dc3a0172984a2f21f802901
Schedul Xor <schedul.xor@gmail.com>
2017-02-06 17:48:19 +0900

    libGDX1.9.5, GWT2.8.0 applied to vtm-web (#290)

---
+++ vtm-web/src/org/oscim/gdx/emu/javax/annotation/CheckReturnValue.java
@@ -0,0 +1,8 @@
+package javax.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target( { ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.PACKAGE })
+public @interface CheckReturnValue {
+}
