6b4bb7d8bfc145129d740569ea80317610f28367
Mathieu De Brito <mathieu.debrito@gmail.com>
2017-05-22 19:28:11 +0200

    Added accessible static value as default building height (#351)

--- vtm/src/org/oscim/layers/tile/buildings/BuildingLayer.java
+++ vtm/src/org/oscim/layers/tile/buildings/BuildingLayer.java
@@ -42,6 +42,7 @@ public class BuildingLayer extends Layer implements TileLoaderThemeHook {
 
     private final static boolean POST_AA = false;
     public static boolean TRANSLUCENT = true;
+    public static int DEFAULT_HEIGHT = 12;
 
     private static final Object BUILDING_DATA = BuildingLayer.class.getName();
 
@@ -87,7 +88,7 @@ public class BuildingLayer extends Layer implements TileLoaderThemeHook {
 
         /* 12m default */
         if (height == 0)
-            height = 12 * 100;
+            height = DEFAULT_HEIGHT * 100;
 
         ExtrusionBuckets ebs = get(tile);
 
