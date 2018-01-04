edfbae323f0168cc78d055e754d91a7b820f453f
Emux <devemux86@gmail.com>
2018-01-04 22:06:08 +0200

    OpenMapTilesGeojsonTileSource: fix header date #468

--- vtm-json/src/org/oscim/tiling/source/geojson/OpenMapTilesGeojsonTileSource.java
+++ vtm-json/src/org/oscim/tiling/source/geojson/OpenMapTilesGeojsonTileSource.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2017 devemux86
  * Copyright 2017 Gustl22
- * Copyright 2017 boldtrn
+ * Copyright 2018 boldtrn
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
