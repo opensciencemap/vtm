43c9c4c8bdb002246bda9597bc5a24eaad90ff98
Emux <devemux86@gmail.com>
2018-01-22 11:56:06 +0200

    mapbox-vector-tile-java (JitPack) #481

--- docs/Integration.md
+++ docs/Integration.md
@@ -87,10 +87,16 @@ implementation 'com.squareup.okio:okio:1.13.0'
 - Mapbox vector tiles
 
 ```groovy
+repositories {
+    maven { url 'https://jitpack.io' }
+}
+```
+
+```groovy
 implementation 'org.mapsforge:vtm-mvt:[CURRENT-VERSION]'
+implementation 'com.github.wdtinc:mapbox-vector-tile-java:master-SNAPSHOT'
 implementation 'com.google.protobuf:protobuf-java:3.5.1'
-implementation 'com.vividsolutions:jts:1.13'
-implementation 'com.wdtinc:mapbox-vector-tile:2.0.0'
+implementation 'org.locationtech.jts:jts-core:1.15.0'
 ```
 
 - GeoJSON vector tiles
--- vtm-mvt/src/org/oscim/tiling/source/mvt/MvtTileDecoder.java
+++ vtm-mvt/src/org/oscim/tiling/source/mvt/MvtTileDecoder.java
@@ -18,20 +18,20 @@
  */
 package org.oscim.tiling.source.mvt;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.MultiLineString;
-import com.vividsolutions.jts.geom.MultiPoint;
-import com.vividsolutions.jts.geom.MultiPolygon;
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.Polygon;
 import com.wdtinc.mapbox_vector_tile.adapt.jts.MvtReader;
 import com.wdtinc.mapbox_vector_tile.adapt.jts.TagKeyValueMapConverter;
 import com.wdtinc.mapbox_vector_tile.adapt.jts.model.JtsLayer;
 import com.wdtinc.mapbox_vector_tile.adapt.jts.model.JtsMvt;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.MultiLineString;
+import org.locationtech.jts.geom.MultiPoint;
+import org.locationtech.jts.geom.MultiPolygon;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
 import org.oscim.core.MapElement;
 import org.oscim.core.Tag;
 import org.oscim.core.Tile;
