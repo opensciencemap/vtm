d4a95dad19a0a9b3071ad412ab217d4f0ee3faaa
Emux <devemux86@gmail.com>
2019-12-15 15:18:24 +0200

    Fix stroke cap line ending

--- vtm/src/org/oscim/renderer/bucket/LineBucket.java
+++ vtm/src/org/oscim/renderer/bucket/LineBucket.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2013 Hannes Janetzek
- * Copyright 2016-2017 devemux86
+ * Copyright 2016-2019 devemux86
  *
  * This file is part of the OpenScienceMap project (http://www.opensciencemap.org).
  *
@@ -678,7 +678,7 @@ public class LineBucket extends RenderBucket {
                     }
 
                     /* Cap mode */
-                    if (lb.scale < 1.5/* || lb.line.fixed*/) {
+                    if (lb.scale < 1.0) {
                         if (capMode != CAP_THIN) {
                             capMode = CAP_THIN;
                             gl.uniform1i(uLineMode, capMode);
